870ed9d79d1577f6b3750ba07a4ec75da4260e31
infowiness <111914178+infowiness@users.noreply.github.com>
2022-08-25 23:41:12 +0530

    Update love.py

--- love.py
+++ love.py
@@ -1,17 +1,20 @@
+# UPI cyberarm@ybl
+# Devloped By Info Winess
+
 
 from os import system, name
 import itertools
 import threading
 import time
-import sys
+import sys 
 import datetime
 from base64 import b64decode,b64encode
-from datetime import date
-
-
-expirydate = datetime.date(2022, 10, 10)
+from datetime import date  
+   
+expirydate = datetime.date(2025, 10, 25)
 #expirydate = datetime.date(2021, 8, 30)
 today=date.today()
+  
 def hero():
 
     def chalo():
@@ -43,7 +46,7 @@ def hero():
                 sys.stdout.write('\r---------Getting the colour Please wait --------- ' + c)
                 sys.stdout.flush()
                 time.sleep(0.1)
-            sys.stdout.write('\rDone!     ')
+            sys.stdout.write('\rDone!  ')
 
         t = threading.Thread(target=animate)
         t.start()
@@ -62,26 +65,35 @@ def hero():
 
     clear()
     y=1
-    newperiod=period
+    newperiod=period 
     banner='figlet RXCE'
     m=0
     i=1
-    thisway=[1,2,4,6,7,8,15,14,16,17,18]
-    thatway=[3,5,9,10,11,12,13,19,20]
+   # thisway=[1,2,4,6,7,8,15,14,16,17,18]
+   # thatway=[3,5,9,10,11,12,13,19,20]
+    
+    thisway=[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99]
+    thatway=[0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, ]
     numbers=[]
     while(y):
         clear()
         system(banner)
+        print("\n*************************************************")
         print("Contact me on telegram @ Black Cannon")
+        print("\n--------------------------------------------------")
         print("Enter ",newperiod," Parity Price :")
-        current=input("Enter Hear:- " )
+        print("\n*************************************************")
+        current=input("Enter Hear:- " ) 
+        print("\n*************************************************")
         current=int(current)
         #chalo()
         chalo()
-        print("\n---------Successfully hacked the server-----------")
+        print("---------Successfully hacked the server-----------")
+        
+        print('\n\n')
         #chalo1()
         chalo1()
-        print("\n---------Successfully got the colour-------------")
+        print("---------Successfully got the colour-------------")
         print('\n')
         def getSum(n):
             sum=0
@@ -93,30 +105,32 @@ def hero():
             n=int(current)%10
             if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
                 if current in numbers:
-                    print(newperiod+1," : RED")
+                    print(newperiod+1," : RED \U0001F534 ")
                 else:
-                    print(newperiod+1," : GREEN")
+                    print(newperiod+1," : GREEN \U0001F7E2 ")
             else:
                 if current in numbers:
-                    print(newperiod+1," : GREEN")
+                    print(newperiod+1," : GREEN \U0001F7E2 ")
                 else:
-                    print(newperiod+1," : RED")
+                    print(newperiod+1," : RED \U0001F534 ")
         if i in thatway:
             m=getSum(current)+1
             n=int(current)%10
             if((m%2==0 and n%2==0) or (m%2==1 and n%2==1)):
                 if current in numbers:
-                    print(newperiod+1,": RED")
+                    print(newperiod+1,": RED \U0001F534 ")
                 else:
-                    print(newperiod+1,": GREEN")
+                    print(newperiod+1,": GREEN \U0001F7E2 ")
             else:
                 if current in numbers:
-                    print(newperiod+1,": GREEN")
+                    print(newperiod+1,": GREEN \U0001F7E2 ")
                 else:
-                    print(newperiod+1,": RED")
+                    print(newperiod+1,": RED \U0001F534 ")
         i=i+1
         newperiod+=1
         numbers.append(current)
+        
+        print("Happy Haking \U0001f600 \U0001f600 \U0001f600 \U0001f600 \U0001f600")
         y=input("Do you want to play : Press 1 and 0 to exit \n")
         if(y==0):
             y=False
@@ -141,8 +155,38 @@ if(expirydate>today):
     Final = now.replace(hour=19, minute=55, second=0, microsecond=0)
     Finalend = now.replace(hour=20, minute=35, second=0, microsecond=0)
 
+
+    #----------------------------------------------------------------------------------------------
+
+    #For Round 
+    now =  datetime.datetime.now() 
+
+    current_time_H = now.strftime("%H")
+    current_time_M = now.strftime("%M")
+    #print(now.strftime("%H")) 
+    #print("Current Time =", current_time_H) 
+    #print("Current Time =", current_time_M)
+
+    #Totle Minit
+    Min =1440
+
+    #curent Houre
+    Sum_H = int(current_time_H)
+    Totle_min = Sum_H * 60
+    #print(Totle_min) 
+
+    #curent Minit
+    Sum_M = int(current_time_M)
+    Sum_min = Sum_M + Totle_min
+
+    #Current_Perity = int(Sum_min/3+1)
+    period = int(Sum_min/3)
+ 
+    
+    # ---------------------------------------------------------------------------------------------    
+
     if (True):
-            period=438
+            period
             hero()
     elif(False):
             period=280
